91cc3a2a3502f35aacdf14a064f306d27491bd9c
bilalkhan141414 <bkhan52302@gmail.com>
2021-02-13 03:17:20 +0500

    First Commit

---
+++ helper.js
@@ -0,0 +1,21 @@
+const encodeString = (stringMessage) => {
+// const mess = "incresing buffer size a bit snowman'☃'"
+const bLength = Buffer.byteLength(stringMessage);
+const buffArray = Buffer.alloc(bLength);
+buffArray.write(stringMessage);
+return buffArray;
+// console.log(bLength, buffArray.toString("UTF-8"));
+}
+
+const decodeString = (bufferArray) => {
+    return bufferArray.toString();
+}
+const getMessage = (respObj)=>{
+    let respMessage = JSON.stringify(respObj);
+    return encodeString(respMessage);
+}
+module.exports = {
+    getMessage, 
+    decodeString,
+    encodeString
+}
---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const socketSr = require("socket.io");
+const http = require("http");
+const cors = require('cors');
+
+const {
+    addUser, 
+    removeUser, 
+    getAllUsers, 
+    getUser, 
+    getUserInRoom
+} = require('./users');
+
+const {
+    encodeString,
+    decodeString,
+    getMessage,
+} = require('./helper');
+const PORT = process.env.PORT || 5000;
+
+const router = require("./router");
+
+
+const app = express();
+
+app.use(router)
+app.use(cors())
+
+const server = http.createServer(app);
+
+const io = socketSr(server,{
+    cors: {
+        origin: '*',
+    }
+  });
+
+//Routes
+
+io.on("connection", (socket)=>{
+
+    console.log("we havee a new connection!!!!");
+
+    socket.on("join", ({name, room}, callback)=>{
+        console.log(name,room);
+        const {error, user} = addUser({id:socket.id, name, room});
+        
+        if(error) return callback(error);
+        socket.emit('message', getMessage({user:"admin", text:`${user.name}, welcome to the room`}));
+        socket.broadcast.to(user.room).emit("message", getMessage({user:'admin', text:`${user.name}, has joind!`}))
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on("sendMessage", (message, callback)=>{
+        console.log(decodeString(message));
+        const user = getUser(socket.id);
+
+        console.log(user, getAllUsers(),socket.id);
+        socket.to(user.room).emit("message", getMessage({user:user.name, text:decodeString(message)}))
+        callback();
+    })
+    socket.on('disconnect', ()=>{
+        const user = removeUser(socket.id);
+        if(user)
+        console.log(`${user.name} had left !!!!`);
+    });
+});
+
+
+server.listen(PORT, ()=>console.log(`Server has starte on port ${PORT}`));
