d09ae9426ad56055f45f8e9d3039f2d9df6fa62d
bilalkhan141414 <bkhan52302@gmail.com>
2021-02-13 15:38:17 +0500

    try catch

--- router.js
+++ router.js
@@ -7,7 +7,7 @@ router.get('/', (req, res)=> {
     res.send("Server is up and running SOON YOU WILL BE ABLE TO SEE THE STATS");
 });
 
-router.get('/routes', (req, res)=> {
+router.get('/logs', (req, res)=> {
     fs.readFile('log.txt', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
